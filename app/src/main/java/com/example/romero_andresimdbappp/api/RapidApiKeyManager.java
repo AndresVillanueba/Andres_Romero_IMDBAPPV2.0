bc7d9d7df8df1c818cfda5c72727570a0fd93dd0
AndresVillanueba <andresjulian.romerovillanueva@riberadeltajo.es>
2025-02-24 00:54:07 +0100

    Funcionalidad 8:Editor de datos de usuarios activos en la aplicacion cambio de api key ya que se me ha agotado

--- app/src/main/java/com/example/romero_andresimdbappp/api/RapidApiKeyManager.java
+++ app/src/main/java/com/example/romero_andresimdbappp/api/RapidApiKeyManager.java
@@ -9,6 +9,7 @@ public class RapidApiKeyManager {
     public RapidApiKeyManager() {
         // Lista de API Keys
         apiKeys = new ArrayList<>();
+        apiKeys.add(0, "74c895ec50msh5642581f6a63ff0p1dd44fjsn0b0b34f8152a"); // Nueva clave al inicio
         apiKeys.add("c1cce6d145msh19937f212457748p163fd5jsnb262629a6f45");
         apiKeys.add("d1f8e6f145msh1b437f21c434648p203fd7jsna4e536a2a9a6");
         apiKeys.add("e8cc36e245msh11237f219122518p905fd1jsnd423822b4f82");
--- app/src/main/java/com/example/romero_andresimdbappp/utils/MyApp.java
+++ app/src/main/java/com/example/romero_andresimdbappp/utils/MyApp.java
@@ -8,10 +8,16 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
         // Inicializa Places usando la API key definida en strings.xml
         String apiKey = getString(R.string.google_maps_key);
         if (!Places.isInitialized()) {
             Places.initialize(getApplicationContext(), apiKey);
         }
+
+        // Registra el AppLifecycleManager para recibir callbacks del ciclo de vida de las Activities
+        AppLifecycleManager lifecycleManager = new AppLifecycleManager(this);
+        registerActivityLifecycleCallbacks(lifecycleManager);
+        registerComponentCallbacks(lifecycleManager);
     }
 }
